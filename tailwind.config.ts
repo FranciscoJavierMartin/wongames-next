a83e7a53c0c39a0e782fb4df993064406385a888
Francisco <javiermartincuenca@gmail.com>
2024-05-25 18:58:19 +0200

    Add colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,27 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    colors: {
+      primary: '#F231A5',
+      secondary: '#3CD3C1',
+      mainBg: '#06092B',
+      white: '#FAFAFA',
+      black: '#030517',
+      lightGray: '#EAEAEA',
+      gray: '#8F8F8F',
+      darkGray: '#2E2F42',
+    },
+    extend: {
+      spacing: {
+        xxsmall: '0.8rem',
+        xsmall: '1.6rem',
+        small: '2.4rem',
+        medium: '3.2rem',
+        large: '4.0rem',
+        xlarge: '4.8rem',
+        xxlarge: '5.6rem',
+      },
+    },
   },
   plugins: [],
 };
