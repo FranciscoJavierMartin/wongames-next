3e0317bfe7cba02aa21f5b56b4d4513165ec8b64
Francisco <javiermartincuenca@gmail.com>
2024-05-26 09:40:17 +0200

    Add stories

--- src/components/Logo/Logo.stories.tsx
+++ src/components/Logo/Logo.stories.tsx
@@ -1,5 +1,5 @@
 import { Meta, StoryObj } from '@storybook/react';
-import Logo, { type LogoProps } from '.';
+import Logo from '.';
 
 const meta: Meta<typeof Logo> = {
   title: 'Logo',
@@ -12,3 +12,43 @@ const meta: Meta<typeof Logo> = {
 export default meta;
 
 export const Default: StoryObj<typeof Logo> = {};
+
+export const Black: StoryObj<typeof Logo> = {
+  args: {
+    color: 'black',
+  },
+};
+
+export const White: StoryObj<typeof Logo> = {
+  args: {
+    color: 'white',
+  },
+};
+
+export const Normal: StoryObj<typeof Logo> = {
+  args: {
+    color: 'black',
+    size: 'normal',
+  },
+};
+
+export const Large: StoryObj<typeof Logo> = {
+  args: {
+    color: 'black',
+    size: 'large',
+  },
+};
+
+export const ShowOnMobile: StoryObj<typeof Logo> = {
+  args: {
+    color: 'black',
+    hideOnMobile: false,
+  },
+};
+
+export const HideOnMobile: StoryObj<typeof Logo> = {
+  args: {
+    color: 'black',
+    hideOnMobile: true,
+  },
+};
