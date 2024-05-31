50899f5a3b9db9cbf5348817c8c9a6aeda31f221
Francisco <javiermartincuenca@gmail.com>
2024-05-31 19:19:28 +0200

    WIP

--- src/components/Heading/Heading.test.tsx
+++ src/components/Heading/Heading.test.tsx
@@ -2,11 +2,29 @@ import { render, screen } from '@testing-library/react';
 import Heading from '.';
 
 describe('<Heading/>', () => {
-  it('should render the heading', () => {
+  it('should render a white heading by default', () => {
     render(<Heading>Most popular</Heading>);
 
     expect(
       screen.getByRole('heading', { name: /most popular/i }),
     ).toBeInTheDocument();
+
+    expect(screen.getByRole('heading', { name: /most popular/i })).toHaveClass(
+      'text-white',
+    );
+  });
+
+  it('should render a black heading when color is passed', () => {
+    render(<Heading color='black'>Most popular</Heading>);
+    expect(screen.getByRole('heading', { name: /most popular/i })).toHaveClass(
+      'text-black',
+    );
+  });
+
+  it('should render a heading with a line to the left side', () => {
+    render(<Heading lineLeft>Most popular</Heading>);
+    expect(screen.getByRole('heading', { name: /most popular/i })).toHaveClass(
+      'text-black',
+    );
   });
 });
