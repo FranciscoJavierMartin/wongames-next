e8015f87440599a17a0b81145383c9edd7615128
Francisco <javiermartincuenca@gmail.com>
2024-06-08 18:43:11 +0200

    Add background style

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -9,16 +9,13 @@ export default function Button({ children, size = 'medium' }: ButtonProps) {
   return (
     <button
       className={clsx([
-        'rounded border-0 py-xxsmall text-white',
+        'rounded border-0 bg-gradient-to-b from-[#ff5f5f] from-0% to-[#f062c0] to-50% py-xxsmall text-white',
         {
           'h-[3rem] px-xxsmall text-xsmall': size === 'small',
           'h-large px-medium text-small': size === 'medium',
           'h-[5rem] px-xlarge text-medium': size === 'large',
         },
       ])}
-      style={{
-        background: 'linear-gradient(180deg, #ff5f5f 0%, #f062c0 50%)',
-      }}
     >
       {!!children && <span>{children}</span>}
     </button>
