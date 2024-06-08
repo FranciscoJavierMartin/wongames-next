15127e12b534b7831083b71c1b106059729d9aaf
Francisco <javiermartincuenca@gmail.com>
2024-06-08 18:52:45 +0200

    Medium test

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -2,11 +2,17 @@ import { render, screen } from '@testing-library/react';
 import Button from '.';
 
 describe('<Button/>', () => {
-  it('should render the heading', () => {
-    render(<Button />);
+  it('should render the medium size by default', () => {
+    render(<Button>Buy now</Button>);
 
     expect(
-      screen.getByRole('heading', { name: /wongames/i }),
+      screen.getByRole('button', { name: /buy now/i }),
     ).toBeInTheDocument();
+
+    expect(screen.getByRole('button', { name: /buy now/i })).toHaveStyle({
+      height: '4.0rem',
+      padding: '0.8rem 3.2rem 0.8rem 3.2rem',
+      'font-size': '1.4rem',
+    });
   });
 });
