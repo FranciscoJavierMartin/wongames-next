78e9b46ad95326b03c164d27462f24f3dd55c6a3
Francisco <javiermartincuenca@gmail.com>
2024-06-08 18:33:29 +0200

    Style button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import { PropsWithChildren } from 'react';
 
 export type ButtonProps = PropsWithChildren<{
@@ -5,5 +6,21 @@ export type ButtonProps = PropsWithChildren<{
 }>;
 
 export default function Button({ children, size = 'medium' }: ButtonProps) {
-  return <button>{!!children && <span>{children}</span>}</button>;
+  return (
+    <button
+      className={clsx([
+        'rounded border-0 py-xxsmall text-white',
+        {
+          'h-[3rem] px-xxsmall text-xsmall': size === 'small',
+          'h-large px-medium text-small': size === 'medium',
+          'h-[5rem] px-xlarge text-medium': size === 'large',
+        },
+      ])}
+      style={{
+        background: 'linear-gradient(180deg, #ff5f5f 0%, #f062c0 50%)',
+      }}
+    >
+      {!!children && <span>{children}</span>}
+    </button>
+  );
 }
